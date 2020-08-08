cb2c285cc24fef3a667ad3447459531083fa6559
sokkunnorn <sokkun.norn2019@gmail.com>
2020-08-08 17:26:34 +0700

    update code page about

--- page.php
+++ page.php
@@ -48,26 +48,22 @@ include('includes/config.php');
 <!--Header-->
 <?php include('includes/header.php');?>
                       <?php 
-$pagetype=$_GET['type'];
-$sql = "SELECT type,detail,PageName from tblpages where type=:pagetype";
-$query = $dbh -> prepare($sql);
-$query->bindParam(':pagetype',$pagetype,PDO::PARAM_STR);
-$query->execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
-$cnt=1;
-if($query->rowCount() > 0)
-{
+$pagetype = $_GET['type'];
+$query = "SELECT type, detail, PageName from tblpages where type= '$pagetype'";
+        
+$results = mysqli_query($conn, $query);
+
 foreach($results as $result)
 { ?>
 <section class="page-header aboutus_page">
   <div class="container">
     <div class="page-header_wrap">
       <div class="page-heading">
-        <h1><?php   echo htmlentities($result->PageName); ?></h1>
+        <h1><?php   echo $result['PageName']; ?></h1>
       </div>
       <ul class="coustom-breadcrumb">
         <li><a href="#">Home</a></li>
-        <li><?php   echo htmlentities($result->PageName); ?></li>
+        <li><?php   echo $result['PageName']; ?></li>
       </ul>
     </div>
   </div>
@@ -79,10 +75,10 @@ foreach($results as $result)
     <div class="section-header text-center">
 
 
-      <h2><?php   echo htmlentities($result->PageName); ?></h2>
-      <p><?php  echo $result->detail; ?> </p>
+      <h2><?php echo $result['PageName']; ?></h2>
+      <p><?php echo $result['detail']; ?> </p>
     </div>
-   <?php } }?>
+   <?php } ?>
   </div>
 </section>
 <!-- /About-us--> 
