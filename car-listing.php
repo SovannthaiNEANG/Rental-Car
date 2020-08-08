25d068a3559e121b0c1b8b0fd86b2417e59a2113
sokkunnorn <sokkun.norn2019@gmail.com>
2020-08-08 18:06:27 +0700

    update code on car list

--- car-listing.php
+++ car-listing.php
@@ -77,41 +77,42 @@ error_reporting(0);
           <div class="sorting-count">
 <?php 
 //Query for Listing count
-$sql = "SELECT id from tblvehicles";
-$query = $dbh -> prepare($sql);
-$query->bindParam(':vhid',$vhid, PDO::PARAM_STR);
-$query->execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
-$cnt=$query->rowCount();
+$query = "SELECT count(*) as cntVehicle from tblvehicles";
+$result = mysqli_query($conn, $query);
+$row = mysqli_fetch_array($result);
+
+$cnt = $row['cntUser'];
+
 ?>
-<p><span><?php echo htmlentities($cnt);?> Listings</span></p>
+<p><span><?php echo $cnt;?> Listings</span></p>
 </div>
 </div>
 
-<?php $sql = "SELECT tblvehicles.*,tblbrands.BrandName,tblbrands.id as bid  from tblvehicles join tblbrands on tblbrands.id=tblvehicles.VehiclesBrand";
-$query = $dbh -> prepare($sql);
-$query->execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
-$cnt=1;
-if($query->rowCount() > 0)
-{
+<?php 
+$query = "SELECT tblvehicles.*, tblbrands.BrandName, tblbrands.id as bid  
+                from tblvehicles 
+                join tblbrands 
+                on tblbrands.id = tblvehicles.VehiclesBrand";
+                
+$results = mysqli_query($conn, $query);
+
 foreach($results as $result)
 {  ?>
         <div class="product-listing-m gray-bg">
-          <div class="product-listing-img"><img src="admin/img/vehicleimages/<?php echo htmlentities($result->Vimage1);?>" class="img-responsive" alt="Image" /> </a> 
+          <div class="product-listing-img"><img src="admin/img/vehicleimages/<?php echo $result['Vimage1'];?>" class="img-responsive" alt="Image" /> </a> 
           </div>
           <div class="product-listing-content">
-            <h5><a href="vehical-details.php?vhid=<?php echo htmlentities($result->id);?>"><?php echo htmlentities($result->BrandName);?> , <?php echo htmlentities($result->VehiclesTitle);?></a></h5>
-            <p class="list-price">$<?php echo htmlentities($result->PricePerDay);?> Per Day</p>
+            <h5><a href="vehical-details.php?vhid=<?php echo $result['id'];?>"><?php echo $result['BrandName'];?> , <?php echo $result['VehiclesTitle'];?></a></h5>
+            <p class="list-price">$<?php echo $result['PricePerDay'];?> Per Day</p>
             <ul>
-              <li><i class="fa fa-user" aria-hidden="true"></i><?php echo htmlentities($result->SeatingCapacity);?> seats</li>
-              <li><i class="fa fa-calendar" aria-hidden="true"></i><?php echo htmlentities($result->ModelYear);?> model</li>
-              <li><i class="fa fa-car" aria-hidden="true"></i><?php echo htmlentities($result->FuelType);?></li>
+              <li><i class="fa fa-user" aria-hidden="true"></i><?php echo $result['SeatingCapacity'];?> seats</li>
+              <li><i class="fa fa-calendar" aria-hidden="true"></i><?php echo $result['ModelYear'];?> model</li>
+              <li><i class="fa fa-car" aria-hidden="true"></i><?php echo $result['FuelType'];?></li>
             </ul>
-            <a href="vehical-details.php?vhid=<?php echo htmlentities($result->id);?>" class="btn">View Details <span class="angle_arrow"><i class="fa fa-angle-right" aria-hidden="true"></i></span></a>
+            <a href="vehical-details.php?vhid=<?php echo $result['id'];?>" class="btn">View Details <span class="angle_arrow"><i class="fa fa-angle-right" aria-hidden="true"></i></span></a>
           </div>
         </div>
-      <?php }} ?>
+      <?php } ?>
          </div>
       
       <!--Side-Bar-->
@@ -126,17 +127,12 @@ foreach($results as $result)
                 <select class="form-control" name="brand">
                   <option>Select Brand</option>
 
-                  <?php $sql = "SELECT * from  tblbrands ";
-$query = $dbh -> prepare($sql);
-$query->execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
-$cnt=1;
-if($query->rowCount() > 0)
-{
-foreach($results as $result)
-{       ?>  
-<option value="<?php echo htmlentities($result->id);?>"><?php echo htmlentities($result->BrandName);?></option>
-<?php }} ?>
+                  <?php $query = "SELECT * from  tblbrands ";
+                  $results = mysqli_query($conn, $query);
+foreach($results as $result) {       
+?>  
+<option value="<?php echo $result['id'];?>"><?php echo $result['BrandName'];?></option>
+<?php } ?>
                  
                 </select>
               </div>
@@ -162,23 +158,24 @@ foreach($results as $result)
           </div>
           <div class="recent_addedcars">
             <ul>
-<?php $sql = "SELECT tblvehicles.*,tblbrands.BrandName,tblbrands.id as bid  from tblvehicles join tblbrands on tblbrands.id=tblvehicles.VehiclesBrand order by id desc limit 4";
-$query = $dbh -> prepare($sql);
-$query->execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
-$cnt=1;
-if($query->rowCount() > 0)
-{
+<?php $query = "SELECT tblvehicles.*, tblbrands.BrandName, tblbrands.id as bid  
+                from tblvehicles 
+                join tblbrands 
+                on tblbrands.id = tblvehicles.VehiclesBrand 
+                order by id desc limit 4";
+
+$results = mysqli_query($conn, $query);
+
 foreach($results as $result)
 {  ?>
 
               <li class="gray-bg">
-                <div class="recent_post_img"> <a href="vehical-details.php?vhid=<?php echo htmlentities($result->id);?>"><img src="admin/img/vehicleimages/<?php echo htmlentities($result->Vimage1);?>" alt="image"></a> </div>
-                <div class="recent_post_title"> <a href="vehical-details.php?vhid=<?php echo htmlentities($result->id);?>"><?php echo htmlentities($result->BrandName);?> , <?php echo htmlentities($result->VehiclesTitle);?></a>
-                  <p class="widget_price">$<?php echo htmlentities($result->PricePerDay);?> Per Day</p>
+                <div class="recent_post_img"> <a href="vehical-details.php?vhid=<?php echo $result['id'];?>"><img src="admin/img/vehicleimages/<?php echo $result['Vimage1'];?>" alt="image"></a> </div>
+                <div class="recent_post_title"> <a href="vehical-details.php?vhid=<?php echo $result['id'];?>"><?php echo $re['BrandName'];?> , <?php echo $re['VehiclesTitle'];?></a>
+                  <p class="widget_price">$<?php echo $result['PricePerDay'];?> Per Day</p>
                 </div>
               </li>
-              <?php }} ?>
+              <?php } ?>
               
             </ul>
           </div>
