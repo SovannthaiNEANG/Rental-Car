0c2fae8b93d3ae324f23f35e7bb4772d11bd70b4
sokkunnorn <sokkun.norn2019@gmail.com>
2020-08-11 01:07:42 +0700

    update manage subcribers

--- admin/manage-subscribers.php
+++ admin/manage-subscribers.php
@@ -2,21 +2,15 @@
 session_start();
 error_reporting(0);
 include('includes/config.php');
-if(strlen($_SESSION['alogin'])==0)
-	{	
-header('location:index.php');
-}
-else{
-if(isset($_GET['del']))
-{
-$id=$_GET['del'];
-$sql = "delete from  tblsubscribers  WHERE id=:id";
-$query = $dbh->prepare($sql);
-$query -> bindParam(':id',$id, PDO::PARAM_STR);
-$query -> execute();
-$msg="Subscriber info deleted";
-
-}
+if (strlen($_SESSION['alogin']) == 0) {	
+	header('location:index.php');
+} else {
+	if (isset($_GET['del'])) {
+		$id=$_GET['del'];
+		$query = "delete from tblsubscribers  WHERE id = $id";
+		$result = mysqli_query($conn, $query);
+		$msg="Subscriber info deleted";
+	}
 
 
  ?>
@@ -88,64 +82,49 @@ $msg="Subscriber info deleted";
 						<div class="panel panel-default">
 							<div class="panel-heading">Subscribers Details</div>
 							<div class="panel-body">
-							<?php if($error){?><div class="errorWrap"><strong>ERROR</strong>:<?php echo htmlentities($error); ?> </div><?php } 
-				else if($msg){?><div class="succWrap"><strong>SUCCESS</strong>:<?php echo htmlentities($msg); ?> </div><?php }?>
+							<?php if($error){?><div class="errorWrap"><strong>ERROR</strong>:<?php echo $error; ?> </div><?php } 
+				else if($msg){?><div class="succWrap"><strong>SUCCESS</strong>:<?php echo $msg; ?> </div><?php }?>
 								<table id="zctb" class="display table table-striped table-bordered table-hover" cellspacing="0" width="100%">
 									<thead>
 										<tr>
-										<th>#</th>
-												<th>Email Id</th>
+											<th>#</th>
+											<th>Email Id</th>
 											<th>Subscription Date</th>
 											<th>Action</th>
 										</tr>
 									</thead>
 									<tfoot>
 										<tr>
-										<th>#</th>
-										<th>Email Id</th>
-										<th>Subscription Date</th>
+											<th>#</th>
+											<th>Email Id</th>
+											<th>Subscription Date</th>
 											<th>Action</th>
 										</tr>
 									</tfoot>
 									<tbody>
 
-									<?php $sql = "SELECT * from tblsubscribers";
-$query = $dbh -> prepare($sql);
-$query->execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
-$cnt=1;
-if($query->rowCount() > 0)
-{
-foreach($results as $result)
-{				?>	
+									<?php $query = "SELECT * from tblsubscribers";
+											$result = mysqli_query($conn, $query);
+											$cnt=1;
+											foreach ($results as $result) {				
+									?>	
 										<tr>
-											<td><?php echo htmlentities($cnt);?></td>
-											<td><?php echo htmlentities($result->SubscriberEmail);?></td>
-									
-											<td><?php echo htmlentities($result->PostingDate);?></td>
-
-										<td>
-
-
-<a href="manage-subscribers.php?del=<?php echo $result->id;?>" onclick="return confirm('Do you want to delete');"><i class="fa fa-close"></i></a>
-</td>
-
+											<td><?php echo $cnt;?></td>
+											<td><?php echo $result['SubscriberEmail'];?></td>
+											<td><?php echo $result['PostingDate'];?></td>
+											<td>
+												<a href="manage-subscribers.php?del=<?php echo $result['id'];?>" onclick="return confirm('Do you want to delete');">
+													<i class="fa fa-close"></i>
+												</a>
+											</td>
 										</tr>
-										<?php $cnt=$cnt+1; }} ?>
-										
+										<?php $cnt++; } ?>
 									</tbody>
 								</table>
-
-						
-
 							</div>
 						</div>
-
-					
-
 					</div>
 				</div>
-
 			</div>
 		</div>
 	</div>
