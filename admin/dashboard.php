4afb1f9db6951d1efa16c22f137dd0632252dd2a
sokkunnorn <sokkun.norn2019@gmail.com>
2020-08-10 23:05:46 +0700

    fix dashboard

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -2,12 +2,10 @@
 session_start();
 error_reporting(0);
 include('includes/config.php');
-if(strlen($_SESSION['alogin'])==0)
-	{	
-header('location:index.php');
-}
-else{
-	?>
+if (strlen($_SESSION['alogin']) == 0) {	
+	header('location:index.php');
+} else {
+?>
 <!doctype html>
 <html lang="en" class="no-js">
 
@@ -42,8 +40,8 @@ else{
 <body>
 <?php include('includes/header.php');?>
 
-	<div class="ts-main-content">
-<?php include('includes/leftbar.php');?>
+<div class="ts-main-content">
+	<?php include('includes/leftbar.php'); ?>
 		<div class="content-wrapper">
 			<div class="container-fluid">
 
@@ -60,13 +58,11 @@ else{
 											<div class="panel-body bk-primary text-light">
 												<div class="stat-panel text-center">
 <?php 
-$sql ="SELECT id from tblusers ";
-$query = $dbh -> prepare($sql);
-$query->execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
-$regusers=$query->rowCount();
+	$query = "SELECT count(id) as cnt from tblusers";
+	$result = mysqli_query($conn, $query);
+	$row = mysqli_fetch_array($result);
 ?>
-													<div class="stat-panel-number h1 "><?php echo htmlentities($regusers);?></div>
+													<div class="stat-panel-number h1 "><?php echo $row['cnt'];?></div>
 													<div class="stat-panel-title text-uppercase">Reg Users</div>
 												</div>
 											</div>
@@ -77,14 +73,12 @@ $regusers=$query->rowCount();
 										<div class="panel panel-default">
 											<div class="panel-body bk-success text-light">
 												<div class="stat-panel text-center">
-												<?php 
-$sql1 ="SELECT id from tblvehicles ";
-$query1 = $dbh -> prepare($sql1);;
-$query1->execute();
-$results1=$query1->fetchAll(PDO::FETCH_OBJ);
-$totalvehicle=$query1->rowCount();
+<?php 
+	$query = "SELECT count(id) as cnt from tblvehicles";
+	$result = mysqli_query($conn, $query);
+	$row = mysqli_fetch_array($result);
 ?>
-													<div class="stat-panel-number h1 "><?php echo htmlentities($totalvehicle);?></div>
+													<div class="stat-panel-number h1 "><?php echo $row['cnt'];?></div>
 													<div class="stat-panel-title text-uppercase">Listed Vehicles</div>
 												</div>
 											</div>
@@ -96,14 +90,12 @@ $totalvehicle=$query1->rowCount();
 											<div class="panel-body bk-info text-light">
 												<div class="stat-panel text-center">
 <?php 
-$sql2 ="SELECT id from tblbooking ";
-$query2= $dbh -> prepare($sql2);
-$query2->execute();
-$results2=$query2->fetchAll(PDO::FETCH_OBJ);
-$bookings=$query2->rowCount();
+	$query ="SELECT count(id) as cnt from tblbooking";
+	$result = mysqli_query($conn, $query);
+	$row = mysqli_fetch_array($result);
 ?>
 
-													<div class="stat-panel-number h1 "><?php echo htmlentities($bookings);?></div>
+													<div class="stat-panel-number h1 "><?php echo $row['cnt'];?></div>
 													<div class="stat-panel-title text-uppercase">Total Bookings</div>
 												</div>
 											</div>
@@ -115,13 +107,11 @@ $bookings=$query2->rowCount();
 											<div class="panel-body bk-warning text-light">
 												<div class="stat-panel text-center">
 <?php 
-$sql3 ="SELECT id from tblbrands ";
-$query3= $dbh -> prepare($sql3);
-$query3->execute();
-$results3=$query3->fetchAll(PDO::FETCH_OBJ);
-$brands=$query3->rowCount();
+	$query = "SELECT count(id) as cnt from tblbrands";
+	$result = mysqli_query($conn, $query);
+	$row = mysqli_fetch_array($result);
 ?>												
-													<div class="stat-panel-number h1 "><?php echo htmlentities($brands);?></div>
+													<div class="stat-panel-number h1 "><?php echo $row['cnt'];?></div>
 													<div class="stat-panel-title text-uppercase">Listed Brands</div>
 												</div>
 											</div>
@@ -148,14 +138,12 @@ $brands=$query3->rowCount();
 											<div class="panel-body bk-primary text-light">
 												<div class="stat-panel text-center">
 <?php 
-$sql4 ="SELECT id from tblsubscribers ";
-$query4 = $dbh -> prepare($sql4);
-$query4->execute();
-$results4=$query4->fetchAll(PDO::FETCH_OBJ);
-$subscribers=$query4->rowCount();
+$query = "SELECT count(id) as cnt from tblsubscribers";
+$result = mysqli_query($conn, $query);
+$row = mysqli_fetch_array($result);
 ?>
-													<div class="stat-panel-number h1 "><?php echo htmlentities($subscribers);?></div>
-													<div class="stat-panel-title text-uppercase">Subscibers</div>
+													<div class="stat-panel-number h1 "><?php echo $row['cnt'];?></div>
+													<div class="stat-panel-title text-uppercase">Subscribes</div>
 												</div>
 											</div>
 											<a href="manage-subscribers.php" class="block-anchor panel-footer">Full Detail <i class="fa fa-arrow-right"></i></a>
@@ -165,14 +153,12 @@ $subscribers=$query4->rowCount();
 										<div class="panel panel-default">
 											<div class="panel-body bk-success text-light">
 												<div class="stat-panel text-center">
-												<?php 
-$sql6 ="SELECT id from tblcontactusquery ";
-$query6 = $dbh -> prepare($sql6);;
-$query6->execute();
-$results6=$query6->fetchAll(PDO::FETCH_OBJ);
-$query=$query6->rowCount();
+<?php 
+	$query = "SELECT count(id) as cnt from tblcontactusquery";
+	$result = mysqli_query($conn, $query);
+	$row = mysqli_fetch_array($result);
 ?>
-													<div class="stat-panel-number h1 "><?php echo htmlentities($query);?></div>
+													<div class="stat-panel-number h1 "><?php echo $row['cnt'];?></div>
 													<div class="stat-panel-title text-uppercase">Queries</div>
 												</div>
 											</div>
@@ -184,14 +170,12 @@ $query=$query6->rowCount();
 											<div class="panel-body bk-info text-light">
 												<div class="stat-panel text-center">
 <?php 
-$sql5 ="SELECT id from tbltestimonial ";
-$query5= $dbh -> prepare($sql5);
-$query5->execute();
-$results5=$query5->fetchAll(PDO::FETCH_OBJ);
-$testimonials=$query5->rowCount();
+	$query = "SELECT count(id) as cnt from tbltestimonial";
+	$result = mysqli_query($conn, $query);
+	$row = mysqli_fetch_array($result);
 ?>
 
-													<div class="stat-panel-number h1 "><?php echo htmlentities($testimonials);?></div>
+													<div class="stat-panel-number h1 "><?php echo $row['cnt'];?></div>
 													<div class="stat-panel-title text-uppercase">Testimonials</div>
 												</div>
 											</div>
@@ -204,15 +188,6 @@ $testimonials=$query5->rowCount();
 						</div>
 					</div>
 				</div>
-
-
-
-
-
-
-
-
-
 			</div>
 		</div>
 	</div>
