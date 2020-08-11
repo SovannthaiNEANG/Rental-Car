d47e72217e6dda0101a2cc3a65465f8027503850
sokkunnorn <sokkun.norn2019@gmail.com>
2020-08-11 19:48:27 +0700

    update code query on booking

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -99,7 +99,7 @@ if (strlen($_SESSION['alogin']) == 0) {
 													<div class="stat-panel-title text-uppercase">Total Bookings</div>
 												</div>
 											</div>
-											<a href="manage-bookings.php" class="block-anchor panel-footer text-center">Full Detail &nbsp; <i class="fa fa-arrow-right"></i></a>
+											<a href="new-bookings.php" class="block-anchor panel-footer text-center">Full Detail &nbsp; <i class="fa fa-arrow-right"></i></a>
 										</div>
 									</div>
 									<div class="col-md-3">
